c454beb3d52cf6f6fe0ec7af2259b5b941447655
ViveK Dutta <vivekd@berkeley.edu>
2023-05-15 19:42:36 -0700

    Add pinecone.ts file

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -1,13 +1,9 @@
 if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error('.env file is missing a variable: PINECONE_INDEX_NAME');
 }
+console.log(process.env.PINECONE_INDEX_NAME)
 
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
-const PINECONE_NAMESPACE = 'demo-ramakrishnan';
+const PINECONE_NAME_SPACE = 'demo11-ramakrishnan';
 
-export { PINECONE_INDEX_NAME, PINECONE_NAMESPACE };
-
-
-
-
-  
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
