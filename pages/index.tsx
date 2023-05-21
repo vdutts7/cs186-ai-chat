2aa6cc074ac66a729fb7019c4609fde635208653
ViveK Dutta <vivekd@berkeley.edu>
2023-05-21 10:53:25 -0700

    types chat issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { useRef, useState, useEffect, useMemo } from 'react';
 import Layout from '@/components/layout';
 import styles from '@/styles/Home.module.css';
-import { Message } from '@/types/chat';
 import { fetchEventSource } from '@microsoft/fetch-event-source';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
+import { Message } from '@/types/chat';
 
 export default function Home() {
   const [query, setQuery] = useState<string>('');
