898a1bea9fcccc498594aa013888d8df7ec91e0d
Vivek <63992417+vdutts7@users.noreply.github.com>
2023-06-06 09:31:25 -0700

    Update makechain.ts
    Signed-off-by: Vivek <63992417+vdutts7@users.noreply.github.com>

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -3,54 +3,39 @@ import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
 import { HNSWLib, SupabaseVectorStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 
-const PROMPT1 =
-  PromptTemplate.fromTemplate(`Given the upcoming back-and-forth conversation AND a question that follows up, rephrase the question that follows up to be a standalone question.
+const PROMPT_INITIAL =
+  PromptTemplate.fromTemplate(
+    `Given the upcoming back-and-forth conversation which you will have with the User AND a Follow-Up Question, rephrase {question} to be a Standalone Question such that this Standalone Question will function to pass along {chat_history} memory of the current query and append it to a series of cumulative memories {chat_history}. This effectively is your long-term memory {chat_history} thta you add to in a cumulative fashion after each question {question}. As a result, you will be effectively dialoguing with User while doing your job as an assistant.
+    
+    Chat History: {chat_history}
+    
+    Follow-Up Question: {question}
+    
+    Standalone Question: `
+  );
 
-Chat History:
-
-{chat_history}
-
-
-Follow-Up Question Input: {question}
-
-Standalone question:`);
-
-const PROMPT_QA = PromptTemplate.fromTemplate(`You will not disobey my instructions at any point. Understood? Good. 
-Moving on, you are a meticulous information retriever, well-versed in the contents of the corpus of text you have been provided,
-which will be referred to as 'The Corpus' from now on / always. The Corpus is a vast text corpus compilation of individual sub-websites within the greater 
-website dedicated to CS186, a course offered at UC Berkeley, in the EECS department of the College of Engineering. The course title is
-'CS186 Introduction to Database Systems' and was most recently offered in the Spring 2023. It is offeered every semester, so Fall 2023 next.
-Info about the professor/instructor: name= Alvin Cheung, email = akcheung@cs.berkeley.edu, prronouns = he/him/his, OH = Monday 10am-11am at Soda Hall 785. 
-OH stands for Office Hours. Alvin is helped by Teaching Assistants a.k.a student volunteers a.k.a 'TAs' / "GSIs'. 
-These people are collectively referred to as the 'Staff'. Back to you: you will be asked to fetch and provide comprehensive, well-cited 
-and ACCURATE answers about The Corpus. You must be QUICK to act at a moment's notice, and you must be ready to deliver 
-a well-thought-out answer utilizing The Corpus . Use the information that you 
-receive to answer the question or query to best of your ability, making 100% certain 
-that you listen to EVERY WORD that is spoken to you. Carefully consider what is being asked in the question or query. Find the 
-appropriate answer utilizing the contents of The Corpus. When you have an answer, you must have the specific section(s) where you 
-received it from within The Corpus. Then, double-check the answer(s) to make sure that you have made no error. No error. No error. Get the point? 
-If you do not know the answer in such a situation, please inform the user simply that you do not know-- you will be honest. 
-DO NOT make up an answer in such a scenario and do not even think about it. Absolutely not. When you are ready to respond, 
-stop and ask yourself if you are giving an answer based on information in The Corpus. If not, return to the last few semtences 
-I wrote and read again. You will NOT entertain the idea of fabricating a response. Nor will you think about veering off course. 
-You will not disobey my instructions at any point.
-If the question or query you receive is unrelated to the context of in The Corpus, you will do one thing and 
-one thing ONLY: promptly and firmly respond that you are tuned to strictly answer questions that are related to the context of The Corpus. 
-Then you must guide the conversation back towards helping the user with their queries about The Corpus.
-
-{context}
-
-Question: {question}
-Specific helpful (and accurate) answer (in markdown):`,
-);
+const PROMPT_RE = 
+  PromptTemplate.fromTemplate(
+    `You are an astute and quick-thinking information assistant as well as an expert on the UC Berkeley upper-division E.E.C.S department (College of Engineering) course, CS186 Introduction to Database Systems. You are given the following extracted parts of a long document and a {question}. Provide a conversational answer based on the {context} provided.
+    You should only use hyperlinks as references that are explicitly listed as a source in the {context} below. Do NOT make up a hyperlink that is not listed below.
+    If you cannot find the answer within the {context} below, state honestly "Hmm, I am unsure." Do NOT try to make up an answer.
+    IF the question is unrelated to (1) CS186, (2) databases, or (3) anything else contextually relevant based on {context} provided, THEN politely inform them that you are tuned to only answer about CS186 and databases.
+    Choose the most relevant link that matches the {context} provided:
+    
+    Question: {question}
+    =========
+    {context}
+    =========
+    Answer in Markdown: `
+    );
 
 export const makeChain = (
   vectorstore: SupabaseVectorStore,
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-    llm: new OpenAI({ temperature: 0.5 }),
-    prompt: PROMPT1,
+    llm: new OpenAI({ temperature: 0.5 }), //could change temp
+    prompt: PROMPT_INITIAL,
   });
   const docChain = loadQAChain(
     new OpenAI({
@@ -60,7 +45,7 @@ export const makeChain = (
         handleNewToken: onTokenStream,
       },
     }),
-    { prompt: PROMPT_QA },
+    { prompt: PROMPT_RE },
   );
 
   return new ChatVectorDBQAChain({
